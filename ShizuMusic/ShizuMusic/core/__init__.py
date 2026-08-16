5f0e552667621f796908259d3cce5e581b506799
trueaura1289-eng <true.aura1289@gmail.com>
2026-08-16 10:29:06 +0530

    Delete ShizuMusic directory

--- ShizuMusic/ShizuMusic/core/__init__.py
+++
@@ -1,7 +0,0 @@
-# --------------------------------------------------------------------------------
-#  ShizuMusic © 2026
-#  Developed by Bad Munda ❤️
-#
-#  Unauthorized copying, editing, re-uploading or removing credits
-#  from this source code is strictly prohibited.
-# --------------------------------------------------------------------------------
