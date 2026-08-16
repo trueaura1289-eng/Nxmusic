c9f707f1bf97b6aec01abf6dab5ddf0887d3c987
trueaura1289-eng <true.aura1289@gmail.com>
2026-08-16 11:46:11 +0530

    Update __init__.py

--- ShizuMusic/core/__init__.py
+++ ShizuMusic/core/__init__.py
@@ -1,7 +1,1 @@
-# --------------------------------------------------------------------------------
-#  ShizuMusic © 2026
-#  Developed by Bad Munda ❤️
-#
-#  Unauthorized copying, editing, re-uploading or removing credits
-#  from this source code is strictly prohibited.
-# --------------------------------------------------------------------------------
+Elyx Music
