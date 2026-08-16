c2bfb7b82b84006eda817de9fe71b35e03bc400a
trueaura1289-eng <true.aura1289@gmail.com>
2026-08-16 12:13:02 +0530

    Update autoplay.py

--- ShizuMusic/modules/autoplay.py
+++ ShizuMusic/modules/autoplay.py
@@ -1,11 +1,3 @@
-# --------------------------------------------------------------------------------
-#  ShizuMusic © 2026
-#  Developed by Bad Munda ❤️
-#
-#  Unauthorized copying, editing, re-uploading or removing credits
-#  from this source code is strictly prohibited.
-# --------------------------------------------------------------------------------
-
 import asyncio
 
 from pyrogram import filters
@@ -39,11 +31,11 @@ async def autoplay_cmd(_, message: Message) -> None:
     user    = message.from_user
 
     if not await is_user_authorized(message):
-        await message.reply(
-            "<b>❍ ᴀᴅᴍɪɴ ᴏɴʟʏ</b>\n"
-            "<b>❍ ᴏɴʟʏ ɢʀᴏᴜᴘ ᴀᴅᴍɪɴꜱ ᴄᴀɴ ᴜꜱᴇ /autoplay</b>",
-            parse_mode=ParseMode.HTML,
-        )
+       await message.reply(
+    "<b>❖ ᴘʟᴀʏ ᴀᴄᴄᴇss ʀᴇsᴛʀɪᴄᴛᴇᴅ</b>\n"
+    "<b>❖ /ᴀᴜᴛᴏᴘʟᴀʏ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ᴏɴʟʏ ғᴏʀ ɢʀᴏᴜᴘ ᴀᴅᴍɪɴs</b>",
+    parse_mode=ParseMode.HTML,
+)
         return
 
     match = message.matches[0]
@@ -52,25 +44,25 @@ async def autoplay_cmd(_, message: Message) -> None:
     if not query:
         current_q = get_autoplay_query(chat_id)
         if is_autoplay(chat_id) and current_q:
-            await message.reply(
-                f"<b>❍ ᴀᴜᴛᴏᴘʟᴀʏ ɪꜱ ᴀʟʀᴇᴀᴅʏ ʀᴜɴɴɪɴɢ</b>\n"
-                f"<b>❍ Qᴜᴇʀʏ :</b> <code>{current_q}</code>\n"
-                f"<b>❍ ᴜꜱᴇ /end ᴛᴏ ꜱᴛᴏᴘ ᴀᴜᴛᴏᴘʟᴀʏ ꜰɪʀꜱᴛ</b>",
-                parse_mode=ParseMode.HTML,
-            )
+         await message.reply(
+    f"<b>❖ ᴀᴜᴛᴏᴘʟᴀʏ ɪs ᴀʟʀᴇᴀᴅʏ ᴀᴄᴛɪᴠᴇ</b>\n"
+    f"<b>❖ ᴄᴜʀʀᴇɴᴛ ǫᴜᴇʀʏ :</b> <code>{current_q}</code>\n"
+    f"<b>❖ ᴜsᴇ /ᴇɴᴅ ᴛᴏ ᴅɪsᴀʙʟᴇ ᴀᴜᴛᴏᴘʟᴀʏ ʙᴇғᴏʀᴇ sᴛᴀʀᴛɪɴɢ ᴀɢᴀɪɴ</b>",
+    parse_mode=ParseMode.HTML,
+)
         else:
-            await message.reply(
-                "<b>❍ ᴜꜱᴀɢᴇ :</b> <code>/autoplay sidhu moose wala</code>\n"
-                "<b>❍ ᴛʜɪꜱ ᴡɪʟʟ ᴄᴏɴᴛɪɴᴜᴏᴜꜱʟʏ ᴘʟᴀʏ ꜱᴏɴɢꜱ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ</b>",
-                parse_mode=ParseMode.HTML,
-            )
+           await message.reply(
+    "<b>❖ ᴀᴜᴛᴏᴘʟᴀʏ ᴄᴏᴍᴍᴀɴᴅ :</b> <code>/autoplay Tere liye</code>\n"
+    "<b>❖ ᴇɴᴛᴇʀ ᴀ ᴛᴏᴘɪᴄ ᴀɴᴅ ʟᴇᴛ ᴛʜᴇ ʙᴏᴛ ᴋᴇᴇᴘ ᴛʜᴇ ᴍᴜsɪᴄ ɢᴏɪɴɢ</b>",
+    parse_mode=ParseMode.HTML,
+)
         return
 
     pm = await message.reply(
-        f"<b>🔁 ꜱᴇᴛᴛɪɴɢ ᴜᴘ ᴀᴜᴛᴏᴘʟᴀʏ...</b>\n"
-        f"<b>❍ Qᴜᴇʀʏ :</b> <code>{query}</code>",
-        parse_mode=ParseMode.HTML,
-    )
+    f"<b>❖ ᴀᴜᴛᴏᴘʟᴀʏ ɪs ʙᴇɪɴɢ ᴘʀᴇᴘᴀʀᴇᴅ</b>\n"
+    f"<b>❖ sᴇᴀʀᴄʜɪɴɢ ғᴏʀ :</b> <code>{query}</code>",
+    parse_mode=ParseMode.HTML,
+)
 
     req    = user.first_name if user else "AutoPlay"
     req_id = user.id         if user else 0
@@ -81,26 +73,26 @@ async def autoplay_cmd(_, message: Message) -> None:
     if count == 0:
         stop_autoplay(chat_id)
         await pm.edit_text(
-            "<b>❍ ᴀᴜᴛᴏᴘʟᴀʏ ꜰᴀɪʟᴇᴅ</b>\n"
-            "<b>❍ ɴᴏ ꜱᴏɴɢꜱ ᴡᴇʀᴇ ꜰᴏᴜɴᴅ, ᴘʟᴇᴀꜱᴇ ᴛʀʏ ᴀɢᴀɪɴ</b>",
-            parse_mode=ParseMode.HTML,
-        )
+    "<b>❖ ᴄᴏᴜʟᴅɴ'ᴛ sᴛᴀʀᴛ ᴀᴜᴛᴏᴘʟᴀʏ</b>\n"
+    "<b>❖ ᴛʜᴇʀᴇ ᴡᴀs ɴᴏ ᴛʀᴀᴄᴋ ᴍᴀᴛᴄʜɪɴɢ ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ</b>",
+    parse_mode=ParseMode.HTML,
+)
         return
 
     first = peek_current(chat_id)
 
-    await pm.edit_text(
-        f"<b>🔁 ᴀᴜᴛᴏᴘʟᴀʏ ꜱᴛᴀʀᴛᴇᴅ</b>\n"
-        f"<b>❍ Qᴜᴇʀʏ :</b> <code>{query}</code>\n"
-        f"<b>❍ {count} ꜱᴏɴɢꜱ ᴀᴅᴅᴇᴅ ᴛᴏ Qᴜᴇᴜᴇ</b>\n"
-        f"<b>❍ ᴜꜱᴇ /end ᴛᴏ ꜱᴛᴏᴘ ᴀᴜᴛᴏᴘʟᴀʏ</b>",
-        parse_mode=ParseMode.HTML,
-    )
+   await pm.edit_text(
+    f"<b>❖ ᴀᴜᴛᴏᴘʟᴀʏ ɪs ɴᴏᴡ ʟɪᴠᴇ</b>\n"
+    f"<b>❖ sᴇᴀʀᴄʜ :</b> <code>{query}</code>\n"
+    f"<b>❖ {count} ᴛʀᴀᴄᴋs ᴀᴅᴅᴇᴅ ᴛᴏ ᴛʜᴇ ᴘʟᴀʏʟɪsᴛ</b>\n"
+    f"<b>❖ ᴛʏᴘᴇ /ᴇɴᴅ ᴛᴏ ᴅɪsᴀʙʟᴇ ᴀᴜᴛᴏᴘʟᴀʏ</b>",
+    parse_mode=ParseMode.HTML,
+)
 
     if not was_playing and first:
         dm = await bot.send_message(
             chat_id,
-            f"<b>❍ ɴᴏᴡ ᴘʟᴀʏɪɴɢ :</b> <code>{short(first['title'])}</code>",
+            f"<b>❖ ᴄᴜʀʀᴇɴᴛʟʏ ᴘʟᴀʏɪɴɢ :</b> <code>{short(first['title'])}</code>",
             parse_mode=ParseMode.HTML,
         )
         await play_song(chat_id, dm, first)
