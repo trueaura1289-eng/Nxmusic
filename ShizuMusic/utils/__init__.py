812bff4547d9535a8d8fc64f86cebbce493dbdc2
trueaura1289-eng <true.aura1289@gmail.com>
2026-08-16 14:38:06 +0530

    Update __init__.py

--- ShizuMusic/utils/__init__.py
+++ ShizuMusic/utils/__init__.py
@@ -1,11 +1,3 @@
-# --------------------------------------------------------------------------------
-#  ShizuMusic © 2026
-#  Developed by Bad Munda ❤️
-#
-#  Unauthorized copying, editing, re-uploading or removing credits
-#  from this source code is strictly prohibited.
-# --------------------------------------------------------------------------------
-
 from ShizuMusic.utils.permissions import is_user_authorized
 from ShizuMusic.utils.decorators import register_block_middleware
 
