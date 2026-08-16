0877fa1f33fd7f177479298c01f126b47f07c2ab
trueaura1289-eng <true.aura1289@gmail.com>
2026-08-16 10:29:50 +0530

    Add files via upload

---
+++ ShizuMusic/core/__init__.py
@@ -0,0 +1,7 @@
+# --------------------------------------------------------------------------------
+#  ShizuMusic © 2026
+#  Developed by Bad Munda ❤️
+#
+#  Unauthorized copying, editing, re-uploading or removing credits
+#  from this source code is strictly prohibited.
+# --------------------------------------------------------------------------------
