9566393e344940e07c13489f669ccf75523d0e84
trueaura1289-eng <true.aura1289@gmail.com>
2026-08-16 15:33:46 +0530

    Update autoplay.py

--- ShizuMusic/modules/autoplay.py
+++ ShizuMusic/modules/autoplay.py
@@ -44,25 +44,25 @@ async def autoplay_cmd(_, message: Message) -> None:
     if not query:
         current_q = get_autoplay_query(chat_id)
         if is_autoplay(chat_id) and current_q:
-         await message.reply(
-    f"<b>❖ ᴀᴜᴛᴏᴘʟᴀʏ ɪs ᴀʟʀᴇᴀᴅʏ ᴀᴄᴛɪᴠᴇ</b>\n"
-    f"<b>❖ ᴄᴜʀʀᴇɴᴛ ǫᴜᴇʀʏ :</b> <code>{current_q}</code>\n"
-    f"<b>❖ ᴜsᴇ /ᴇɴᴅ ᴛᴏ ᴅɪsᴀʙʟᴇ ᴀᴜᴛᴏᴘʟᴀʏ ʙᴇғᴏʀᴇ sᴛᴀʀᴛɪɴɢ ᴀɢᴀɪɴ</b>",
-    parse_mode=ParseMode.HTML,
-)
+            await message.reply(
+                f"<b>❖ ᴀᴜᴛᴏᴘʟᴀʏ ɪs ᴀʟʀᴇᴀᴅʏ ᴀᴄᴛɪᴠᴇ</b>\n"
+                f"<b>❖ ᴄᴜʀʀᴇɴᴛ ǫᴜᴇʀʏ :</b> <code>{current_q}</code>\n"
+                f"<b>❖ ᴜsᴇ /ᴇɴᴅ ᴛᴏ ᴅɪsᴀʙʟᴇ ᴀᴜᴛᴏᴘʟᴀʏ ʙᴇғᴏʀᴇ sᴛᴀʀᴛɪɴɢ ᴀɢᴀɪɴ</b>",
+                parse_mode=ParseMode.HTML,
+            )
         else:
-           await message.reply(
-    "<b>❖ ᴀᴜᴛᴏᴘʟᴀʏ ᴄᴏᴍᴍᴀɴᴅ :</b> <code>/autoplay Tere liye</code>\n"
-    "<b>❖ ᴇɴᴛᴇʀ ᴀ ᴛᴏᴘɪᴄ ᴀɴᴅ ʟᴇᴛ ᴛʜᴇ ʙᴏᴛ ᴋᴇᴇᴘ ᴛʜᴇ ᴍᴜsɪᴄ ɢᴏɪɴɢ</b>",
-    parse_mode=ParseMode.HTML,
-)
+            await message.reply(
+                "<b>❖ ᴀᴜᴛᴏᴘʟᴀʏ ᴄᴏᴍᴍᴀɴᴅ :</b> <code>/autoplay Tere liye</code>\n"
+                "<b>❖ ᴇɴᴛᴇʀ ᴀ ᴛᴏᴘɪᴄ ᴀɴᴅ ʟᴇᴛ ᴛʜᴇ ʙᴏᴛ ᴋᴇᴇᴘ ᴛʜᴇ ᴍᴜsɪᴄ ɢᴏɪɴɢ</b>",
+                parse_mode=ParseMode.HTML,
+            )
         return
 
     pm = await message.reply(
-    f"<b>❖ ᴀᴜᴛᴏᴘʟᴀʏ ɪs ʙᴇɪɴɢ ᴘʀᴇᴘᴀʀᴇᴅ</b>\n"
-    f"<b>❖ sᴇᴀʀᴄʜɪɴɢ ғᴏʀ :</b> <code>{query}</code>",
-    parse_mode=ParseMode.HTML,
-)
+        f"<b>❖ ᴀᴜᴛᴏᴘʟᴀʏ ɪs ʙᴇɪɴɢ ᴘʀᴇᴘᴀʀᴇᴅ</b>\n"
+        f"<b>❖ sᴇᴀʀᴄʜɪɴɢ ғᴏʀ :</b> <code>{query}</code>",
+        parse_mode=ParseMode.HTML,
+    )
 
     req    = user.first_name if user else "AutoPlay"
     req_id = user.id         if user else 0
@@ -73,21 +73,21 @@ async def autoplay_cmd(_, message: Message) -> None:
     if count == 0:
         stop_autoplay(chat_id)
         await pm.edit_text(
-    "<b>❖ ᴄᴏᴜʟᴅɴ'ᴛ sᴛᴀʀᴛ ᴀᴜᴛᴏᴘʟᴀʏ</b>\n"
-    "<b>❖ ᴛʜᴇʀᴇ ᴡᴀs ɴᴏ ᴛʀᴀᴄᴋ ᴍᴀᴛᴄʜɪɴɢ ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ</b>",
-    parse_mode=ParseMode.HTML,
-)
+            "<b>❖ ᴄᴏᴜʟᴅɴ'ᴛ sᴛᴀʀᴛ ᴀᴜᴛᴏᴘʟᴀʏ</b>\n"
+            "<b>❖ ᴛʜᴇʀᴇ ᴡᴀs ɴᴏ ᴛʀᴀᴄᴋ ᴍᴀᴛᴄʜɪɴɢ ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ</b>",
+            parse_mode=ParseMode.HTML,
+        )
         return
 
     first = peek_current(chat_id)
 
-   await pm.edit_text(
-    f"<b>❖ ᴀᴜᴛᴏᴘʟᴀʏ ɪs ɴᴏᴡ ʟɪᴠᴇ</b>\n"
-    f"<b>❖ sᴇᴀʀᴄʜ :</b> <code>{query}</code>\n"
-    f"<b>❖ {count} ᴛʀᴀᴄᴋs ᴀᴅᴅᴇᴅ ᴛᴏ ᴛʜᴇ ᴘʟᴀʏʟɪsᴛ</b>\n"
-    f"<b>❖ ᴛʏᴘᴇ /ᴇɴᴅ ᴛᴏ ᴅɪsᴀʙʟᴇ ᴀᴜᴛᴏᴘʟᴀʏ</b>",
-    parse_mode=ParseMode.HTML,
-)
+    await pm.edit_text(
+        f"<b>❖ ᴀᴜᴛᴏᴘʟᴀʏ ɪs ɴᴏᴡ ʟɪᴠᴇ</b>\n"
+        f"<b>❖ sᴇᴀʀᴄʜ :</b> <code>{query}</code>\n"
+        f"<b>❖ {count} ᴛʀᴀᴄᴋs ᴀᴅᴅᴇᴅ ᴛᴏ ᴛʜᴇ ᴘʟᴀʏʟɪsᴛ</b>\n"
+        f"<b>❖ ᴛʏᴘᴇ /ᴇɴᴅ ᴛᴏ ᴅɪsᴀʙʟᴇ ᴀᴜᴛᴏᴘʟᴀʏ</b>",
+        parse_mode=ParseMode.HTML,
+    )
 
     if not was_playing and first:
         dm = await bot.send_message(
